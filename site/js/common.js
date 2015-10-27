2507d3704d7ec518f6a5fcb9d5aa4db1af1aa9f2
etna-scorpio <etna.scorpio@gmail.com>
2015-10-27 18:14:25 +0200

    task

--- site/js/common.js
+++ site/js/common.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
 		if ($('.overlay').not('is-active')) {
 			$('.overlay').addClass('is-active');
 		}
-		if (!popup.hasClass('suggest')) {
+		if (!popup.hasClass('js-avoid-overflow')) {
 			$('body').addClass('is-overflow');
 		}
 	};
@@ -58,15 +58,36 @@ $(document).ready(function() {
 		}
 	});
 
-	// tooltip
+	// tooltips
 	$('.js-tooltip').tooltipster({
 		position: 'right',
 		maxWidth: 290
 	});
 
+	$('.js-tooltip-guide').tooltipster({
+		position: 'right',
+		maxWidth: 290,
+		interactive: true,
+		contentAsHTML: true,
+		trigger: 'custom'
+	});
+
+	$('.js-tooltip-guide').tooltipster('show');
+
+	$('.js-tooltip-guide').click(function() {
+		var data = $(this).data('attr');
+		$('.js-tooltip-guide-hide[data-attr="' + data + '"]').parents('.tooltipster-base').toggle();
+	});
+
+	$('.js-tooltip-guide-hide').click(function(e) {
+		e.preventDefault();
+		$(this).parents('.tooltipster-base').hide();
+	});
+
 	// dropdown
 	$('.js-dropit').click(function(e) {
 		e.preventDefault();
+		$('.js-to-drop').removeClass('is-visible');
 		$(this).parent().find('.js-to-drop').toggleClass('is-visible');
 	});
 
@@ -87,6 +108,14 @@ $(document).ready(function() {
 	$('.js-slide-up').click(function(e) {
 		e.preventDefault();
 		$(this).parents('.js-slide-parent').slideUp();
+	});
+
+	// textare autoresize
+	$(document).on('input.textarea', '.js-expand', function() {
+		var minRows = this.getAttribute('data-min-rows') | 0,
+			rows	= this.value.split('\n').length;
+
+		this.rows = rows < minRows ? minRows : rows;
 	});
 
 });
