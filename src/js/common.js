99753b5a69f026e071f0dc6103336e804d40115d
etna-scorpio <etna.scorpio@gmail.com>
2015-10-30 18:08:45 +0200

    new pages

--- src/js/common.js
+++ src/js/common.js
@@ -27,6 +27,7 @@ $(document).ready(function() {
 		var el = $(el);
 		el.on('click', function(event) {
 			event.preventDefault();
+			event.stopPropagation();
 			var popup = $('#' + el.data('popup'));
 			showPopup(popup);
 		});
@@ -46,18 +47,6 @@ $(document).ready(function() {
 		$( 'html, body' ).animate({
 			scrollTop: 0
 		}, 'slow');
-	});
-
-	// detect link on click
-	$('.js-link').click(function(e) {
-		var targetLink = $(this).data('href');
-
-		if ($(e.target).parents('.js-table-drop').length > 0) {
-			e.preventDefault;
-		} else {
-			e.preventDefault;
-			window.location.href = targetLink;
-		}
 	});
 
 	// tooltips
@@ -94,10 +83,11 @@ $(document).ready(function() {
 	});
 
 	$('body').click(function(e) {
-		if ( $(e.target).parents('.js-wrap').length > 0 ) {
+		if ( $(e.target).parents('.js-wrap').length > 0 || $(e.target).parents('.js-table-drop').length > 0) {
 			e.stopPropagation();
 		} else {
 			$('.js-to-drop').removeClass('is-visible');
+			$('.dropdown').removeClass('is-active');
 		}
 	});
 
@@ -105,6 +95,24 @@ $(document).ready(function() {
 	$('.table th').click(function() {
 		$(this).siblings().removeClass('is-active');
 		$(this).addClass('is-active');
+	});
+
+	// detect click on table row
+	$('.js-link').click(function(e) {
+		var targetLink = $(this).data('href');
+		if ($(e.target).parents('.js-table-drop').length > 0) {
+			if (!$(e.target).parents('.dropdown__list').length > 0) {
+				e.preventDefault();
+			}
+		} else {
+			window.location.href = targetLink;
+		}
+	});
+
+	// table dropdown
+	$('.js-table-drop').click(function(e) {
+		$(this).closest('.js-link').siblings().find('.dropdown').removeClass('is-active');
+		$(this).find('.dropdown').toggleClass('is-active');
 	});
 
 	$('.js-slide-up').click(function(e) {
@@ -122,5 +130,20 @@ $(document).ready(function() {
 
 	// bar
 	$('.js-bar').addClass('is-active');
+
+	// actions on deal settings
+	$('.js-action input').change(function() {
+		$('.js-action').removeClass('is-active');
+		$(this).parents('.js-action').addClass('is-active');
+	});
+
+	$('.js-action').click(function() {
+		if (!$(this).hasClass('is-disabled')) {
+			$(this).siblings().removeClass('is-active');
+			$(this).siblings().find('input').prop('checked', false);
+			$(this).find('input').prop('checked', true);
+			$(this).addClass('is-active');
+		}
+	});
 
 });
