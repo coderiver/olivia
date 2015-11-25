cabd43977dfb4c278c0db350bb10270357f29108
etna-scorpio <etna.scorpio@gmail.com>
2015-11-25 16:51:04 +0200

    form + comments, ff popup fx

--- site/js/common.js
+++ site/js/common.js
@@ -15,7 +15,6 @@ $(document).ready(function() {
 
 	$('.js-tooltip-guide').tooltipster({
 		maxWidth: 290,
-		position: 'right',
 		interactive: true,
 		contentAsHTML: true,
 		trigger: 'custom',
@@ -35,16 +34,50 @@ $(document).ready(function() {
 	});
 
 	// comments
+	function commentBl(element) {
+		var input = $(element).find('input'),
+			commentBtn = $(element).find('.btn[type="submit"]'),
+			cancelBtn = $(element).find('.btn[type="button"]'),
+			btnGroup = $(element).find('.btn-group');
+
+		input.on( 'keyup', function() {
+			if ( input.val().length > 0 ) {
+				commentBtn.removeAttr('disabled');
+			} else if (input.val().length === 0 ) {
+				commentBtn.prop('disabled', true);
+			}
+		}).on('focus', function() {
+			btnGroup.addClass('is-visible');
+		});
+
+		cancelBtn.on('click', function() {
+			input.val('');
+			commentBtn.prop('disabled', true);
+		});
+	}
+
+	$('.js-newpost').each(function(index, element) {
+		commentBl(element);
+	});
+
 	$('.js-comment').on('click', function(evt) {
 		evt.preventDefault();
-		var newComment,
-			commentParent = $(this).parent();
+		var commentParent = $(this).parent(),
+			wrapper,
+			inner;
 
-		if ( commentParent.children('.newpost').length === 0 ) {
-			newComment = document.createElement('div');
-			newComment.innerHTML = '<label><input type="text" placeholder="Type your comment/question"></label><div class="btn-group"><button class="btn btn-no s-pad" type="button">Cancel</button><button class="btn btn-ok s-pad" type="submit">Comment</button></div>';
-			newComment.classList.add('newpost');
-			commentParent.append(newComment);
+		if ( commentParent.children('.js-newpost').length === 0 ) {
+			wrapper = $('<div />', {'class': 'newpost js-newpost'}).appendTo(commentParent);
+			inner = $(
+				'<div class="newpost__in">'
+					+ '<label><input type="text" placeholder="Type your comment/question"></label>'
+					+ '<div class="btn-group is-visible">'
+						+ '<button class="btn btn-no m-pad" type="button">Cancel</button>'
+						+ '<button class="btn btn-ok m-pad" type="submit" disabled>Comment</button>'
+					+ '</div>'
+				+ '</div>').appendTo(wrapper);
+			commentParent.children('.js-newpost').find('input').focus();
+			commentBl(wrapper);
 		}
 	});
 
--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,4 @@
 //= lib/jquery.js
-//= lib/slick.min.js
 //= lib/tooltipster.min.js
 //= lib/malihu-scrollbar.min.js
 //= popups.js
