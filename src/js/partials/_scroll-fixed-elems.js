d96eb8499473f8ffe09e4252ca30805cc0ba96a6
Serhii <kso_132@mail.ru>
2018-05-13 22:28:56 +0300

    upd new entry

--- src/js/partials/_scroll-fixed-elems.js
+++ src/js/partials/_scroll-fixed-elems.js
@@ -23,12 +23,10 @@ if(isEntryPage === true){
 	// при position fixed ширина немного меняется
 	let sidebarWidth = $(".js-entry-sidebar").width();
 	$(".js-entry-sidebar").css('width', `${sidebarWidth}px`);
-
-	var sidebarAtBottom = false;
-	var sidebarAtTop;
-	var minSidebarTopOffset = document.querySelector(".js-entry-sidebar").getBoundingClientRect().top;
-	var temp = minSidebarTopOffset;
-	var defaultBottomOffset = Math.abs(document.querySelector(".js-entry-content").getBoundingClientRect().bottom - $(document).height());
+	var globalSidebarPos;
+	// var minSidebarTopOffset = document.querySelector(".js-entry-sidebar").getBoundingClientRect().top;
+	// var temp = minSidebarTopOffset;
+	// var defaultBottomOffset = Math.abs(document.querySelector(".js-entry-content").getBoundingClientRect().bottom - $(document).height());
 
 };
 
@@ -45,18 +43,20 @@ function scrollEntrySidebar(scrollProps){
 
 	let contentBottomPos = entryContent.getBoundingClientRect().bottom;
 	let contentTopPos = entryContent.getBoundingClientRect().top;
-	let sidebarTopPos = entrySidebar.getBoundingClientRect().top;
 
+	let sidebarTopPos = entrySidebar.getBoundingClientRect().top;
+	let sidebarPos = entrySidebar.getBoundingClientRect();
+	let contentPos = entryContent.getBoundingClientRect();
 	let $entrySidebar = $(entrySidebar);
 
 
+	// console.log(`pos from parent ${$entrySidebar.position().top }`);
 
 
-
-	if (minSidebarTopOffset < sidebarTopPos) {
-		minSidebarTopOffset = sidebarTopPos;
-	};
-	let minusTopOffset = -minSidebarTopOffset;
+	// if (minSidebarTopOffset < sidebarTopPos) {
+	// 	minSidebarTopOffset = sidebarTopPos;
+	// };
+	// let minusTopOffset = -minSidebarTopOffset;
 
 
 
@@ -78,57 +78,71 @@ function scrollEntrySidebar(scrollProps){
 		
 		function toBottomScroll(){
 				// $entrySidebar.addClass('to-bottom').removeClass('to-top');
-				let bottomOffset = Math.abs(contentBottomPos - $(document).height());
-				let minBottomOffset =  bottomOffset  < defaultBottomOffset ? bottomOffset : defaultBottomOffset;
+				// let bottomOffset = Math.abs(contentBottomPos - $(document).height());
+				// let minBottomOffset =  bottomOffset  < defaultBottomOffset ? bottomOffset : defaultBottomOffset;
 
-				if(sidebarTopPos + minBottomOffset <= minusTopOffset){
-					$entrySidebar.css({'position': 'fixed', 'bottom': `${minBottomOffset}px`});
-					$entrySidebar.addClass('to-bottom js-control');
-				};
-
-				if(entryContent.getBoundingClientRect().bottom == entrySidebar.getBoundingClientRect().bottom){
-					sidebarAtBottom = true;
-				};
+				// if(sidebarTopPos + minBottomOffset <= minusTopOffset){
 
 
 
-			};
-			function toTopScroll(){
+					// if($entrySidebar.hasClass('is-fixed') ) {
 
+					// }else{
 
-				// if($entrySidebar.hasClass('to-bottom')){
-
-				// 	$entrySidebar.removeClass('to-bottom').addClass("is-translated");
-
-				// 	$entrySidebar.css({'position': 'relative', 'bottom': `auto`, 'transform': `translateY(${temp + currTop}px)`});
-				// 	return;
-				// }
-
-				if($entrySidebar.hasClass('js-control')){
-					$entrySidebar.addClass('to-top');
-					if(sidebarTopPos >= temp){
-
-						$entrySidebar.css({'position': 'fixed', 'bottom': `auto`, 'transform': `translateY(0px)`});
-						$entrySidebar.removeClass('is-translated');
-						if(currTop == 0){
-							$entrySidebar.css({'position': 'relative', 'bottom': `auto`, 'transform': `translateY(0px)`});
-							$entrySidebar.removeClass('is-translated js-control to-top');
-							return;
-						}
+					// };
+					if($entrySidebar.hasClass('is-fixed') && $entrySidebar.hasClass('is-translated')){
+						console.log(1);
+						let translate = currTop + availableContentHeight + sidebarHeight;
+						$entrySidebar.css({'position': 'relative', 'bottom': `auto`, 'transform': `translateY(${translate}px)`});
+						$entrySidebar.removeClass('is-fixed');
+					};
+					if(sidebarPos.bottom - $(window).height() <= 0){
+						// $entrySidebar.css({'position': 'fixed', 'bottom': `${minBottomOffset}px`});
+						$entrySidebar.css({'position': 'fixed', 'bottom': `0px`});
+						$entrySidebar.addClass('is-fixed js-control').removeClass('is-translated');
+						console.log(2);
 					};
 
-					if($entrySidebar.hasClass('is-translated')) return;
-					if(sidebarAtBottom == true){
+
+
+
+					if(contentPos.bottom <= sidebarPos.bottom){
 						let	translateValue = contentHeight - sidebarHeight;
 
 						$entrySidebar.css({'position': 'relative', 'bottom': `auto`, 'transform': `translateY(${translateValue}px)`});
-						$entrySidebar.addClass('is-translated');
-						sidebarAtBottom = false;
-					}
+						$entrySidebar.removeClass('is-fixed').addClass('is-translated');
+						console.log(3);
+					};
+
 
 				};
+				function toTopScroll(){
 
-			};
+
+					if(!$entrySidebar.hasClass('js-control')) return;
+					// $entrySidebar.addClass('to-top');
+					if(sidebarTopPos - headerHeight  >= 0 ){
+						// console.log(`first`);
+						$entrySidebar.css({'position': 'fixed', 'bottom': `auto`, 'transform': `translateY(0px)`});
+						$entrySidebar.addClass('is-fixed');
+						// sidebarAtBottom = false;
+						if(currTop == 0){
+							$entrySidebar.css({'position': 'relative', 'bottom': `auto`, 'transform': `translateY(0px)`});
+							$entrySidebar.removeClass('is-fixed js-control is-translated');
+							// return;
+						}
+					};
+					if($entrySidebar.hasClass('is-translated')) return;
+					if($entrySidebar.hasClass('is-fixed') ) {
+						// console.log(`second`);
+						let translate = currTop + availableContentHeight - sidebarHeight;
+						$entrySidebar.css({'position': 'relative', 'bottom': `auto`, 'transform': `translateY(${translate}px)`});
+						$entrySidebar.removeClass('is-fixed').addClass('is-translated');
+					};
+
+
+				};
+				globalSidebarPos = currTop + availableContentHeight - sidebarHeight;
 
 // end function
 };
