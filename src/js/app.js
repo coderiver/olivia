e82a692f3e4edac991b0c0f525dfa039c67f8baa
Serhii <kso_132@mail.ru>
2018-05-23 14:15:45 +0300

    removed libs

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
-//=include lib/jquery.js
-//=include lib/tooltipster.min.js
-//=include lib/perfect-scrollbar.jquery.min.js
+//include lib/jquery.js
+//include lib/tooltipster.min.js
+//include lib/perfect-scrollbar.jquery.min.js
 //=include common.js
 //=include table.js
 //=include comments.js
