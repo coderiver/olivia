ff08fad60de8b9479fc1b0f281342ead22713823
Serhii <kso_132@mail.ru>
2018-06-06 10:13:39 +0300

    build

--- build/js/app.js
+++ build/js/app.js
@@ -290,16 +290,14 @@ $(document).ready(function() {
 	        content.slideToggle(350);
 	    }
 	});
-	console.log(`entry`);
 	function initEntryOneScreen(){
+		if(!$('.js-entry-content')[0]) return;
 	let content = $('.js-entry-content');
 	let sidebar = $('.js-entry-sidebar');
 	let entryWrapper = $('.js-entry-two-col');
 	let wrapperPos = entryWrapper.offset().top;
 	let windowH = $(window).height();
 	let height = windowH - wrapperPos;
-	console.log(windowH);
-	console.log(wrapperPos);
 	sidebar.css('height', `${height}px`)
 	content.css('height', `${height}px`)
 	};
--- src/js/partials/_entry-one-screen.js
+++ src/js/partials/_entry-one-screen.js
@@ -1,13 +1,11 @@
-console.log(`entry`);
 function initEntryOneScreen(){
+	if(!$('.js-entry-content')[0]) return;
 let content = $('.js-entry-content');
 let sidebar = $('.js-entry-sidebar');
 let entryWrapper = $('.js-entry-two-col');
 let wrapperPos = entryWrapper.offset().top;
 let windowH = $(window).height();
 let height = windowH - wrapperPos;
-console.log(windowH);
-console.log(wrapperPos);
 sidebar.css('height', `${height}px`)
 content.css('height', `${height}px`)
 };
